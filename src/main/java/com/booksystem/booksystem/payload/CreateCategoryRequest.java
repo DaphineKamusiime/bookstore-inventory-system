1a57b2d80fcc1d7a3a48dd7a308868ee8d45d759
Daphine Kamusiime <daphnekamusiime09@gmail.com>
2024-04-19 07:17:50 +0300

    created repository files

---
+++ src/main/java/com/booksystem/booksystem/payload/CreateCategoryRequest.java
@@ -0,0 +1,16 @@
+package com.booksystem.booksystem.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class CreateCategoryRequest {
+    @NotBlank
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
